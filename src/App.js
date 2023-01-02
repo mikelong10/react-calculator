b5a14fb4cfc3e43fe82b1ba67df378e4a5c5d8ff
Michael Long <michaeltlong10@gmail.com>
2023-01-01 21:52:56 -0500

    finished calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import Input from "./components/Input"
+const math = require('mathjs')
 
-function App() {
+export default function Calc() {
+  const [clearStatus, setClearStatus] = useState('AC')
+  const [prevOutput, setPrevOutput] = useState('')
+  const [curOutput, setCurOutput] = useState('')
+
+  function clear(clearStatus) {
+    if (clearStatus === 'AC') {
+      setPrevOutput('')
+      setCurOutput('')
+    } else {
+      setClearStatus('AC')
+      setCurOutput('')
+    }
+  }
+
+  function enterNumber(value) {
+    setClearStatus('C')
+    const newCurOutput = curOutput + value
+    if (newCurOutput.length > 10) {
+      setCurOutput(newCurOutput.substring(0, 10))
+    } else {
+      setCurOutput(newCurOutput)
+    }
+  }
+
+  function doOperation(operation) {
+    setPrevOutput(curOutput.concat(' ', operation))
+    setCurOutput('')
+  }
+
+  function evaluate() {
+    const expression = prevOutput.concat(' ', curOutput).split(' ')
+    setPrevOutput('')
+    // actually do the math
+    const [firstVal, operation, secondVal] = expression
+    doMath(firstVal, operation, secondVal)
+  }
+
+  // to toggle the sign (positive/negative) of the current number
+  function toggleSign() {
+    setCurOutput(math.multiply(Number(curOutput), -1).toString())
+  }
+
+  // calculate percentage version of the current output
+  function percentize() {
+    doMath(curOutput, '/', '100')
+  }
+
+  function doMath(firstVal, operation, secondVal) {
+    let evaluation
+    if (operation === '+') {
+      evaluation = math.add(Number(firstVal), Number(secondVal))
+    } else if (operation === '–' || operation === '-') {
+      evaluation = math.subtract(Number(firstVal), Number(secondVal))
+    } else if (operation === 'x' || operation === '*') {
+      evaluation = math.multiply(Number(firstVal), Number(secondVal))
+    } else if (operation === '÷' || operation === '/') {
+      evaluation = math.divide(Number(firstVal), Number(secondVal))
+    }
+    const answer = evaluation.toString()
+    if (answer.includes('.') && (answer.split('.')[1].substring(0, 6) === '999999')) {
+      setCurOutput(Math.round(evaluation).toString())
+    } else if (answer.length > 10) {
+      setCurOutput(answer.substring(0, 9))
+    } else {
+      setCurOutput(answer)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calc">
+      <div className="output">
+        <div className="prev">{prevOutput}</div>
+        <div className="current">{curOutput}</div>
+      </div>
+      <div className="inputs">
+        <div className="left">
+          <div className="misc">
+            <Input value={clearStatus} onClick={() => clear(clearStatus)} />
+            <Input value={'±'} onClick={toggleSign} />
+            <Input value={'%'} onClick={percentize} />
+          </div>
+          <div className="numbers">
+            <div className="row">
+              <Input value={7} inputType={'digit'} onClick={() => enterNumber('7')} />
+              <Input value={8} inputType={'digit'} onClick={() => enterNumber('8')} />
+              <Input value={9} inputType={'digit'} onClick={() => enterNumber('9')} />
+            </div>
+            <div className="row">
+              <Input value={4} inputType={'digit'} onClick={() => enterNumber('4')} />
+              <Input value={5} inputType={'digit'} onClick={() => enterNumber('5')} />
+              <Input value={6} inputType={'digit'} onClick={() => enterNumber('6')} />
+            </div>
+            <div className="row">
+              <Input value={1} inputType={'digit'} onClick={() => enterNumber('1')} />
+              <Input value={2} inputType={'digit'} onClick={() => enterNumber('2')} />
+              <Input value={3} inputType={'digit'} onClick={() => enterNumber('3')} />
+            </div>
+            <div className="row">
+              <Input value={0} inputType={'digit'} onClick={() => enterNumber('0')} />
+              <Input value={'.'} inputType={'decimal'} onClick={() => enterNumber('.')} />
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="operations">
+            <Input value={'÷'} inputType={'operation'} onClick={() => doOperation('÷')} />
+            <Input value={'x'} inputType={'operation'} onClick={() => doOperation('x')} />
+            <Input value={'–'} inputType={'operation'} onClick={() => doOperation('–')} />
+            <Input value={'+'} inputType={'operation'} onClick={() => doOperation('+')} />
+            <Input value={'='} inputType={'operation'} onClick={evaluate} />
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/Input.js
@@ -0,0 +1,9 @@
+
+
+export default function Input({ value, inputType, onClick }) {
+  if (value === 0) inputType = inputType + ' zero'
+
+  return (
+    <button className={`input ${inputType}`} onClick={onClick}>{value}</button>
+  )
+}
